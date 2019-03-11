ef03c4171d0a9d4dec1054744cfe0cd3f18259cb
Zack LaVergne <zlavergne94@gmail.com>
2019-03-11 15:12:12 -0600

    V0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='changesets2CSV',
-      version='0.1.0',
+      version='0.2.0',
       description='OSM Changesets CSV writer',
       url='https://github.com/KaartGroup/Changesets2CSV',
       author='Kaart',
