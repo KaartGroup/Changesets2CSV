8a6c6276d0ffc0dfce2671276aa6cbfafa12ce70
Zack LaVergne <zlavergne94@gmail.com>
2019-03-11 14:55:39 -0600

    initial commit for setuptools to allow for pip install

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='changeset2CSV',
+      version='0.1.0',
+      description='OSM Changeset CSV writer',
+      url='https://github.com/KaartGroup/Changesets2CSV',
+      author='Kaart',
+      license='MIT',
+      python_requires='>=2.7.0,<3.0.0',
+      scripts=['changeset2CSV.py'])
