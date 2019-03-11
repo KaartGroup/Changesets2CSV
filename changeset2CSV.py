052fe1d9c4c1c1c55499db98db3c007df27568ef
Zack LaVergne <zlavergne94@gmail.com>
2019-03-11 14:55:23 -0600

    Not maxed out at 100 changesets anymore

--- changeset2CSV.py
+++ changeset2CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-''' changesetApi2Csv
+''' changeset2CSV
 
 This tool will build out a CSV of changeset info queried based on the given parameters
 
@@ -14,10 +14,9 @@ import xml.etree.ElementTree as ET
 import csv
 import argparse
 import urllib
-import os
 
 
-def changeset2Csv(file, user=None, start_time=None, end_time=None, bbox=None):
+def changeset2Csv(file_name, user=None, start_time=None, end_time=None, bbox=None):
     query_params = {}
     if user:
         if user.isdigit():
@@ -31,17 +30,50 @@ def changeset2Csv(file, user=None, start_time=None, end_time=None, bbox=None):
     if bbox:
         query_params['bbox'] = ','.join(bbox)
 
-    api_url = "https://api.openstreetmap.org/api/0.6/changesets?" + urllib.urlencode(query_params)
-    changeset = urllib.urlopen(api_url).read()
+    changesets = []
+    try:
+        api_url = "https://api.openstreetmap.org/api/0.6/changesets?" + urllib.urlencode(query_params)
+        result = urllib.urlopen(api_url).read()
+        root = ET.fromstring(result)
+        sets = root.findall('changeset')
+        print(sets[0].get('closed_at'))
+        print(sets[-1].get('closed_at'))
+        changesets.extend(sets)
 
-    with open(file, 'w') as f:
-        fieldnames = ['ID', 'Comment', 'Open', 'User', 'Created at', 'Closed at', 'Changes', 'Discussions']
+        while len(sets) >= 100:
+            print(sets[0].get('id'))
+            print("TRUE")
+            new_end = sets[-1].get('closed_at').split("T")[0]
+            print("\nNEW END: " + new_end)
+            start_time = "1970-01-01" if not start_time else start_time
+            query_params['time'] = ','.join([start_time, new_end])
+            print(query_params)
+            api_url = "https://api.openstreetmap.org/api/0.6/changesets?" + urllib.urlencode(query_params)
+            result = urllib.urlopen(api_url).read()
+            root = ET.fromstring(result)
+            sets = root.findall('changeset')
+            changesets.extend(sets)
+
+    except Exception as e:
+        print("Error with calling the API: " + str(e))
+
+    with open(file_name, 'w') as f:
+        fieldnames = ['ID', 'Comment', 'Open', 'User', 'Created at', 'Closed at', 'Changes', 'Discussions', 'URL']
         csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
+        url = "https://www.openstreetmap.org/changeset/{}"
 
-        root = ET.fromstring(changeset)
+        # root = ET.fromstring(changeset)
         csv_writer.writeheader()
-        for item in root.findall('changeset'):
-            changeset = {'ID': item.get('id'), 'Comment': '', 'Open': item.get('open'), 'User': item.get('user'), 'Created at': item.get('created_at'), 'Closed at': item.get('closed_at'), 'Changes': item.get('changes_count'), 'Discussions': item.get('comments_count')}
+        for item in changesets:
+            changeset = {'ID': item.get('id'),
+                         'Comment': '',
+                         'Open': item.get('open'),
+                         'User': item.get('user'),
+                         'Created at': item.get('created_at'),
+                         'Closed at': item.get('closed_at'),
+                         'Changes': item.get('changes_count'),
+                         'Discussions': item.get('comments_count'),
+                         'URL': url.format(item.get('id'))}
 
             for child in item:
                 if child.get('k') == 'comment':
@@ -51,15 +83,16 @@ def changeset2Csv(file, user=None, start_time=None, end_time=None, bbox=None):
 
     return True
 
-if __name__ == "__main__":        
-    
+
+if __name__ == "__main__":
+
     ''' Set up arguments '''
     parser = argparse.ArgumentParser(description="Create CSV file of changeset info given query parameters.")
     parser.add_argument('output', help="Location and name of the .csv file to create ")
     parser.add_argument('-u', '--user', help="The OSM username or user id to use for the query (either username or user id, NOT both).")
-    parser.add_argument('-s', '--start_time', help="The start time of the window to query (year-month-day).")
-    parser.add_argument('-e', '--end_time', help="The end time of the window to query (year-month-day).")
-    parser.add_argument('-b', '--bbox', nargs=4 , help="The bbox to query changesets. Values separated by spaces (min_lon min_lat max_lon max_lat).")
+    parser.add_argument('-s', '--start_time', help="The start time of the window to query (YYYY-MM-DD).")
+    parser.add_argument('-e', '--end_time', help="The end time of the window to query (YYYY-MM-DD).")
+    parser.add_argument('-b', '--bbox', nargs=4, help="The bbox to query changesets. Values separated by spaces (min_lon min_lat max_lon max_lat).")
     args = parser.parse_args()
 
     if not ((args.user or args.bbox) or (args.start_time and args.end_time)):
