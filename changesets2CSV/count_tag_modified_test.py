86d6fbcbd6d369b55d23d85a70d831e0b6f9f730
Lucas Bingham <droiba@yahoo.com>
2019-08-09 10:36:41 -0600

    count_tag_modified file for testing new difference-finding script

---
+++ changesets2CSV/count_tag_modified_test.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+
+import xml.etree.ElementTree as ET
+import csv
+import argparse
+import requests
+from cachecontrol import CacheControl
+from datetime import datetime, date, timedelta
+import os
+import sys
+import glob
+import re
+import time
+import ssl
+from xlsxwriter.workbook import Workbook
+import doctest
+#from tqdm import tqdm
+#from defusedxml import ElementTree as ElementTree
+
+
+USER_AGENT = "trackDownload/0.1 (lucas.bingham@kaartgroup.com)"
+
+print_version_lists = True
+print_query = False
+print_query_response = False
+object_limit_for_query=0
+dont_run_query = True
+dont_process_query = True
+
+def count_tag_change(changeset,tag, change_type="*", osm_obj_type="*",const_tag="none"):
+  api_url = "https://www.openstreetmap.org/api/0.6/changeset/{changeset}/download".format(changeset=changeset)
+  dev_api_url = "https://master.apis.dev.openstreetmap.org/api/0.6/changeset/{changeset}/download".format(changeset=changeset)
+  api_way_url = "https://www.openstreetmap.org/api/0.6/way/"
+  api_url = api_url
+  session = CacheControl(requests.session())
+  result = session.get(api_url).text
+  root = ET.fromstring(result)
+
+  #Dictionaries of id, value, version
+  new_ver_objects = []
+  if const_tag != "none":
+      #List of 'osm_obj_type's that have tags 'const_tag' and 'tag'
+      objs_with_tags = root.findall("./{change_type}/{osm_obj_type}/tag[@k='{const_tag}']../tag[@k='{tag}']..".format(tag=tag,const_tag=const_tag,osm_obj_type=osm_obj_type,change_type=change_type))
+      objs_without_tags = root.findall("")
+      tags = root.findall("./{change_type}/{osm_obj_type}/tag[@k='{const_tag}']../tag[@k='{tag}']".format(tag=tag,const_tag=const_tag,osm_obj_type=osm_obj_type,change_type=change_type))
+      ids_w_const = []
+      for i in range(len(tags)):
+          new_ver_object = {"id":objs_with_tags[i].attrib["id"],"value":tags[i].attrib["v"],"version":int(objs_with_tags[i].attrib["version"])}
+          new_ver_objects.append(new_ver_object)
+  else:
+      #List of 'osm_obj_type's that have tag 'tag'
+      objs_with_tags = root.findall("./{change_type}/{osm_obj_type}/tag[@k='{tag}']..".format(tag=tag,osm_obj_type=osm_obj_type,change_type=change_type))
+      tags = root.findall("./{change_type}/{osm_obj_type}/tag[@k='{tag}']".format(tag=tag,osm_obj_type=osm_obj_type,change_type=change_type))
+      ids_w_const = []
+      for i in range(len(tags)):
+          new_ver_object = {"id":objs_with_const_tags[i].attrib["id"],"value":tags[i].attrib["v"],"version":int(objs_with_const_tags[i].attrib["version"])}
+          new_ver_objects.append(new_ver_object)
+  if print_version_lists:
+      print("New_Ver: ")
+      for obj in new_ver_objects: print(obj)
+      print()
+
+  query = "[out:json][timeout:25];"
+  query_count = 0
+  for obj in new_ver_objects:
+      #Can this ever happen?
+      if obj["version"] > 1 and (query_count < object_limit_for_query or object_limit_for_query == 0):
+          if object_limit_for_query != 0:
+              print("Object ",query_count+1," of ",object_limit_for_query)
+          query_part = "timeline({osm_obj_type}, {osm_id}, {prev_version}); for (t['created']) {{ retro(_.val) {{ {osm_obj_type}(id:{osm_id}); out meta;}} }}"\
+          .format(osm_obj_type = osm_obj_type, osm_id = obj["id"],prev_version = int(obj["version"])-1)
+          query += query_part
+          query_count += 1
+  if print_query: print(query)
+
+  if dont_run_query == False:
+      query_json = overpass_query(query)
+
+  if print_query_response:
+      if dont_run_query:
+          print("Cannot print query if it was not run")
+      else:
+          print(query_json)
+
+  if dont_process_query == False:
+      #Dictionaries of id, value, version
+      old_ver_objects = []
+      for element in query_json["elements"]:
+          these_tags = element['tags']
+          if these_tags.get(tag,None) != None:
+              old_ver_objects.append({"id":element['id'],"value":element['tags'][tag],"version":element['version']})
+          else:
+              old_ver_objects.append({"id":element['id'],"value":None,"version":element['version']})
+      if print_version_lists:
+          print("Old_Ver: ")
+          for obj in old_ver_objects: print(obj)
+          print()
+
+      #See what values changed
+      changed_values = 0
+      for i in range(len(old_ver_objects)):
+          old_value = old_ver_objects[i]["value"]
+          new_value = new_ver_objects[i]["value"]
+          if old_value != new_value:
+              print(old_value," became ",new_value)
+              changed_values += 1
+          else:
+              print(old_value, "didn't change")
+
+      return changed_values
+  else:
+      return 0
+
+
+
+def overpass_status(api_status_url = "https://overpass-api.de/api/status"):
+    """Get the overpass status -- this returns an int with the time to wait"""
+    session = requests.session()
+    session.headers.update({'User-Agent': USER_AGENT})
+    cached_session = CacheControl(session)
+    response = cached_session.get(api_status_url)
+    if (response.status_code != requests.codes.ok):
+        raise ValueError("Bad Request: {}".format(api_status_url))
+    parsed_response = {'wait_time': []}
+    for i in response.text.splitlines():
+        if "Connected as" in i:
+            parsed_response['connected_as'] = i.split(":")[1].strip()
+        elif "Current time" in i:
+            parsed_response['current_time'] = i.split(":")[1].strip()
+        elif "Rate limit" in i:
+            parsed_response['rate_limit'] = int(i.split(":")[1].strip())
+        elif "slots available now" in i:
+            parsed_response['slots_available'] = int(i.split(" ")[0].strip())
+        elif "Slot available after" in i:
+            parsed_response['wait_time'].append(int(i.split(" ")[5]))
+    if 'slots_available' not in parsed_response:
+        parsed_response['slots_available'] = 0
+    wait_time = 0
+    if parsed_response['rate_limit'] - parsed_response['slots_available'] >= 2 and len(parsed_response['wait_time']) > 0:
+        return max(parsed_response['wait_time'])
+    return wait_time
+
+def overpass_query(query):
+    """Query the overpass servers. This may block for extended periods of time, depending upon the query"""
+
+    session = requests.session()
+    session.headers.update({'User-Agent': USER_AGENT})
+    cached_session = CacheControl(session)
+    response = cached_session.post("http://overpass-api.de/api/interpreter", data={'data': query})
+    wait_time = overpass_status()
+    loop = 0
+    while (wait_time > 0):
+        time.sleep(wait_time)
+        wait_time = overpass_status()
+        loop += 1
+    while (response.status_code == requests.codes.too_many_requests):
+        time.sleep(10)
+        response = cached_session.post("http://overpass-api.de/api/interpreter", data={'data': query})
+    if (response.status_code != requests.codes.ok):
+        print("Bad request")
+        print(response.text)
+        print(response.status_code)
+        raise ValueError("Bad Request: {}".format(query))
+
+    xml = response.text
+
+    if (response.status_code != requests.codes.ok):
+        raise ValueError("We got a bad response code of {} for {} which resulted in:\r\n{}".format(response.status_code, query, xml))
+    content_type = response.headers.get('content-type')
+    if content_type == 'application/osm3s+xml':
+        return ET.ElementTree(ElementTree.fromstring(xml))
+    elif content_type == 'application/json':
+        return response.json()
+    else:
+        raise ValueError("Unexpected content type ({}) from the query: {}".format(content_type, query))
+
+name_changes = count_tag_change(73198966,"name","*","way","highway")
+print(name_changes," names were changed in this changeset")
